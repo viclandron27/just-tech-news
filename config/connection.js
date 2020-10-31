732868f34bc29fee8b2326ddce452ab7e0593bb4
Victoria Landron <viclandron@gmail.com>
2020-10-31 12:33:36 -0400

    created user model

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 //create connection to our database, pass in your MySQL information for username and password
-const sequelize = new Sequelize('just_tech_news_db', 'username', 'password', {
+const sequelize = new Sequelize('just_tech_news_db', 'root', 'Maribel98*', {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
