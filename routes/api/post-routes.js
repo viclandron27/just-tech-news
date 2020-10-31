e8991681fe928b05eb6226870d71572bbac5c934
Victoria Landron <viclandron@gmail.com>
2020-10-31 17:13:35 -0400

    finished setting up voting

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,11 +1,18 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const sequelize = require('../../config/connection');
+const { Post, User, Vote } = require('../../models');
 
 //get all users
 router.get('/', (req, res) => {
     console.log('================');
     Post.findAll({
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: [
+          'id', 
+          'post_url', 
+          'title', 
+          'created_at',
+          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
         order: [['created_at', 'DESC']], 
         include: [
             {
@@ -27,7 +34,13 @@ router.get('/:id', (req, res) => {
       where: {
         id: req.params.id
       },
-      attributes: ['id', 'post_url', 'title', 'created_at'],
+      attributes: [
+        'id', 
+        'post_url', 
+        'title', 
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
       include: [
         {
           model: User,
@@ -63,6 +76,18 @@ router.post('/', (req, res) => {
     });
 });
 
+
+//vote on a post
+router.put('/upvote', (req, res) => {
+    // custom static method created in models/Post.js
+      Post.upvote(req.body, { Vote })
+      .then(updatedPostData => res.json(updatedPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+})
+
 //update a post
 router.put('/:id', (req, res) => {
   Post.update(
@@ -88,6 +113,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
+
 //delete a post
 router.delete('/:id', (req, res) => {
   Post.destroy({
@@ -107,6 +133,5 @@ router.delete('/:id', (req, res) => {
       res.status(500).json(err);
     });
 });
-As before, we
 
 module.exports = router;
