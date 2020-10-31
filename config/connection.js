41e734e52ded94464d15aae579758a9d215846fb
Victoria Landron <viclandron@gmail.com>
2020-10-31 17:37:48 -0400

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -3,11 +3,18 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
+let sequelize;
+
 //create connection to our database, pass in your MySQL information for username and password
-const sequelize = new Sequelize('just_tech_news_db', 'root', 'Maribel98*', {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+}
+else {
+    sequelize = new Sequelize('just_tech_news_db', 'root', 'Maribel98*', {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+}
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
