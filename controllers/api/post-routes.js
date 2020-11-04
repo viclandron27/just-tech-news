7b1b9dba199b734e9ad1c82320a25c3e0fc944bc
Victoria Landron <viclandron@gmail.com>
2020-11-03 21:02:06 -0500

    creating a single post page and rendering objects based on login session

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -87,13 +87,16 @@ router.post('/', (req, res) => {
 
 //vote on a post
 router.put('/upvote', (req, res) => {
-    // custom static method created in models/Post.js
-      Post.upvote(req.body, { Vote })
-      .then(updatedPostData => res.json(updatedPostData))
+    // make sure the session exists first
+  if (req.session) {
+    // pass session id along with all destructured properties on req.body
+    Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+      .then(updatedVoteData => res.json(updatedVoteData))
       .catch(err => {
         console.log(err);
-        res.status(400).json(err);
+        res.status(500).json(err);
       });
+  }
 })
 
 //update a post
